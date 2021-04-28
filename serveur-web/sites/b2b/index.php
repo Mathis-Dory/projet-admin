da79193f6aac55eb5923f3fdb4646234d9c9f8eb
Eliott <64136394+Eliott-Lepage@users.noreply.github.com>
2021-04-29 00:13:27 +0200

    Update index.php

--- serveur-web/sites/b2b/index.php
+++ serveur-web/sites/b2b/index.php
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title> Site B2B l1-2 - Config WEB</title>
+</head>
+<body>
+<p> Responsable du service WEB - Eliott Lepage </p>
+<p>Vérification de la configuration de la db</p>
+        <pre>
+        <?php
+            $query ='SELECT * FROM shopb2b;';
+            try
+            {
+                $db = new PDO('mysql:host=172.17.0.2;dbname=b2bl12', 'fpm', 'mysqlb2b');
+                $answer = $db->query($query);
 
+                foreach($answer->fetchAll(PDO::FETCH_ASSOC) as $row) {
+                print_r($row);
+                }
+                $db = null;
+            }
+            catch (PDOException $e) {
+				echo 'Connection failed: ';
+            }
+
+            echo '<hr><br>';
+            echo '</pre>'; ?>
+<h1>Welcome to our site B2B</h1>
+<?php echo phpinfo(); ?>
+</body>
+</html>
